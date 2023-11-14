54f0c9f743f0fdbc567d695f6876d3390bd23403
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-14 18:25:56 +0200

    update

---
+++ Assignment_5/Memory_Management2/src/memory_management2/Process.java
@@ -0,0 +1,35 @@
+package memory_management2;
+
+public class Process {
+    private int pid;
+    private int arrivalTime;
+    private int burstTime;
+    private double sizeNeeded;
+
+    public Process(int pid, int arrivalTime, int burstTime, double sizeNeeded) {
+        this.pid = pid;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.sizeNeeded = sizeNeeded;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public double getSizeNeeded() {
+        return sizeNeeded;
+    }
+
+    public void decrementBurstTime() {
+        burstTime--;
+    }
+}
