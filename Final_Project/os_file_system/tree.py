98bd634ea1e757e32d1e70ec4860e6f8465df0b8
unknown <you@example.com>
2024-01-04 14:46:34 +0200

    some comments added, commented code removed

--- Final_Project/os_file_system/tree.py
+++ Final_Project/os_file_system/tree.py
@@ -1,5 +1,7 @@
+# to retrieve saved files
 import json
 import math
+# to add the tree in the real OS
 import os
 from random import random
 from datetime import datetime
@@ -18,50 +20,53 @@ class tree:
     def __init__(self, **dict):
         if dict:
             self.root = leaf(**dict)
-            # self.updateSize(self.root)
-            # print("update done")
         else:
+            # we set the root of the tree and give it name, type, and the boolean set to true meaning it is a partition not a normal directory
             self.root = leaf('root', 'directory', True)
+            # default access for the root
             self.root.access = 'srwx'
 
+    """
+    Add file/folder to a directory
+    """
     def addChild(self, Dir, leaf):
         # check if the name exists before creating new file/folder
         if leaf.name in self.ls(Dir):
             print('Duplicated name')
             return
 
-        # check if the user has permissions to write before creating file/folder
-        # if Dir.access[1]!='w':
-        #     print('addChild\nAccess denied')
-        #     return
-
         # changing the path of the new file to be present in the directory and creating it
         leaf.path = Dir.path + f'/{leaf.name}'
         Dir.children.append(leaf)
-        # Dir.size += leaf.size
 
     # check if the permissions are entered as read, write, execute. Also a user cannot write if they can't read the file
     def validAccess(self, a):
-        # if len(a)>3 or a[0] not in ['r', '-'] or a[1] not in ['w', '-'] or a[2] not in ['x', '-'] or (a[1] == 'w' and a[0] == '-'):
         if len(a)!=3 or a[0] not in ['r', '-'] or a[1] not in ['w', '-'] or a[2] not in ['x', '-']:
-        # if len(a)!=4 or a[0] not in ['s', '-'] or a[1] not in ['r', '-'] or a[2] not in ['w', '-'] or a[3] not in ['x', '-']:
             return False
         return True
 
-    # return the names of files/folders in a directory or the content in a file
+    """
+    return the names of files/folders in a directory or the content in a file
+    """    
     def ls(self, leaf):
+        # if it is a file return its content
         if leaf.type !='directory':
             return leaf.content
 
+        # if it's a directory make a list of names
         names = []
+        # iterate children and add each child's name
         for x in leaf.children:
             names.append(x.name)
         return names
 
-    # iterates from root node to our last file
+    """
+    get the file/folder with the path given
+    """
     def parsePath(self, path):
         # change path to a list of steps,  if a step (directory) not found then the path is wrong
         path = path.split('/')
+        # we always start from root so remove it as we know already
         path.remove('root')
         # var root will be our iterator
         root = self.root
@@ -70,26 +75,28 @@ class tree:
         for x in path:
             # catch an error if the file/folder not found and returning the last available folder in the path
             try:
+                # search for the name of the file in the list of names returned by ls(), if found return its index to use in the children list
                 root = root.children[self.ls(root).index(x)]
+                # we accessed the file/folder so we need to update access time
                 root.accessTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                # check if we have the read permission before accessing the file/folder then change accessTime if you have the permission,  O.W return last available folder
-                # if root.children[self.ls(root).index(x)].access[0] == 'r':
-                #     root = root.children[self.ls(root).index(x)]
-                #     root.accessTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                # else:
-                #     print('access denied')
-                #     return root
             except:
                 print('Invalid path')
+                # if we failed, we return the last available node in the path
                 return root
+        # we return the node
         return root
 
+    """
+    check if we have write permission in a node
+    """
     def isWritable(self, f):
         if f.access[2] == 'w':
             return True
         return False
 
-    # print the tree in the console
+    """
+    print the tree in the console
+    """
     def showTree(self, node):
         # we add spaces before file name = the depth of the file (number of folders before it) to create a hierarchy view
         print(' '*(len(node.path.split('/'))-1)*2, node.name)
@@ -98,44 +105,49 @@ class tree:
             for x in node.children:
                 self.showTree(x)
 
-    # same idead as showTree but it writes the files in the real OS
+    """
+    writes the files in the real OS
+    """
     def writeTree(self, node):
+        # if it's a directory we add to the real os using its path (note: its path to the os is relative to our project directory but everything is wrapped in the root directory)
         if node.type == 'directory':
             os.makedirs(node.path)
+            # recursively iterate through children and apply the function
             for x in node.children:
                 self.writeTree(x)
         else:
+            # if it's a text file we create the file and add its content to it
             if node.type == 'text file':
+                # create the file and open it in write mode
                 with open(node.path, 'w') as f:
+                    # add its content
                     f.write(node.content)
             else:
+                # if it's not a text file we do the same but without adding its content because we assumend all contents is text and that's not the case in real files
                 with open(node.path, 'w') as f:
                     f.write('')
 
-
+    """
+    deletes file/folder
+    """
     def delete(self, Dir, name):
+        # if the file is found we execute the function
         if name in self.ls(Dir):
+            # if the file is created by the super user, we ask for the password to delete
             if Dir.access[0] == 's':
                 pas = input('Enter Super User Password: ')
+                # if passwords don't match, don't delete the file
                 if pas != 'FBI':
                     print('Wrong password')
                     return
+            # if the function wasn't stopped due to any of the above conditions then we delete the file and change its directory modification time
             Dir.children.pop(self.ls(Dir).index(name))
             Dir.modifyTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         else:
+            # if the file name we provided doesn't exist tell the user
             print('File does not exist')
             return
-        # we check if write permission is granted and if the file exist in the directory. if so,  we remove the file and modifyTime in the directory
-        # if Dir.access[1] == 'w':
-        #     if name in self.ls(Dir):
-        #         Dir.children.pop(self.ls(Dir).index(name))
-        #         Dir.modifyTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        #     else:
-        #         print('File does not exist')
-        #         return
-        # else:
-        #     print('delete\nAccess denied')
-        #     return
+
 
     def changeAccess(self, file, access):
         # check again if the entered access is the way we parse it
@@ -149,49 +161,75 @@ class tree:
     def getAccess(self, file):
         return file.access
 
-    # pastes a copied object
+    """
+    pastes a copied object
+    """
     def copy(self, Dir, file):
-        # if where we are trying to paste is not a directory we say don't paste it
+        # get the root to change its size after copying a file (because we created a new file that might have content)
         root = self.parsePath('root')
-        if root.totalSize-root.size < len(file.content):
+        # if the file size is bigger than free space, we abort the paste because there is no space
+        if root.totalSize-root.size < file.size:
             print('No enough space')
             return
+        # we can't paste a file/directory inside a file as it has no children
         if Dir.type != 'directory':
             print('operation not possible in a file')
         else:
-            # if name already exist, change to a new name and paste it
+            # if name already exist, change to a new name (copy_old name) and paste it
             if file.name in self.ls(Dir):
                 file.name = 'copy_'+file.name
+            # paste the copy
             self.addChild(Dir, file)
+            # get current date
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            # change file dates because it's a new file
+            # directory has been modified so change its modification time
             Dir.modifyTime = now
+            # that's a new file so a new creation time and the modification and access times will initially be the same as creationg
             file.createTime = now
             file.modifyTime = now
             file.accessTime = now
-            # file.path = Dir.path+'/'+file.name
+            # that's a new file, so a new path and new ID as well
             self.updatePath(Dir,file,'copy')
 
+    """
+    Updates the path of a file after pasting
+    """
     def updatePath(self,Dir,f,o):
+        # the file path is the same as the directory + the file name
         f.path = Dir.path+'/'+f.name
+        # if the operation is a copy so we create a new ID as it's a new file
         if o == 'copy':
+            # getID() creates new unique ID
             f.getID()
-        if f.type == 'directory':
-            for x in f.children:
-                self.updatePath(f,x,'copy')
-    # same as copy function but it asks the user if the want to overwrite the file instead of changing the name
+            # if the pasted node was a directory, recursively update children paths and IDs
+            if f.type == 'directory':
+                for x in f.children:
+                    self.updatePath(f,x,'copy')
+        else:
+            # if the operation wasn't a copy so so we recursively update children paths only
+            if f.type == 'directory':
+                for x in f.children:
+                    self.updatePath(f,x,'cut')
+
+    """
+    same as copy function but it asks the user if the want to overwrite the file instead of changing the name
+    """
     def cut(self, Dir, file):
         if Dir.type != 'directory':
             print('operation not possible')
         else:
+            # if the file name exist, ask the user if they want to overwrite the old file
             if file.name in self.ls(Dir):
                 r = input('File already exist,  overwrite? (Y|N): ')
                 if r == 'Y':
                     Dir.children.remove(Dir.children[self.ls(Dir).index(file.name)])
                 else:
                     return
+            # the old directory where the file/folder came from
             old = self.parsePath(file.path[:file.path.rfind('/')])
+            # remove the file/folder from its old directory
             old.children.remove(old.children[self.ls(old).index(file.name)])
+            # add the file to the new directory
             self.addChild(Dir, file)
 
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -199,66 +237,62 @@ class tree:
             file.createTime = now
             file.modifyTime = now
             file.accessTime = now
-            # file.path = Dir.path+'/'+file.name
             self.updatePath(Dir,file,'cut')
 
     def getContent(self, file):
         return file.content
 
-    #TODO incomplete, we need to properly adjust size
+    """
+    update content of a file
+    """
     def updateContent(self, file, text):
         file.content = text
+        # update the modification time and file size
         file.modifyTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.size = len(file.content)
 
-    # update size of desk
+    """
+    update size of desk
+    """
     def updatesize(self,node):
+        # if the node is a directory, its size will be the sum of its children sizes
        if node.type == 'directory':
+           # if it has children we recursively iterate them
             if len(node.children) > 0:
+                # the total size
                 total=[]
+                # for each child
                 for x in node.children:
-                    total.append(self.updatesize(x))    
+                    # we add its size and its children sizes
+                    total.append(self.updatesize(x))
+                    # the directory is the sum of all sizes
                     node.size=sum(total)
                 return node.size    
             else:
+                # if it has no children, its size is 0
                 node.size=0
                 return 0   
+        # if it's a file, its size is its content length
        else:
-            # if '.' in node.name:
-            #     extension = node.name.split('.')[-1]
-            #     if extension == 'txt':
-            #           node.size=len(node.content)/4
-            #           return node.size 
-            #     if extension in ['jpg', 'jpeg', 'png']:
-            #           node.size=50
-            #           return 50
-            #     if extension == 'mp3':
-            #           node.size=50
-            #           return 50
-            #     if extension == 'mp4':
-            #           node.size=100
-            #           return 100
-            # else:
-            #     return 0
             node.size = len(node.content)
             return node.size
 
 
-    # rename function 
+    """
+    Renames a file/directory
+    """
     def rename(self,root,node,name):
+        # we can't use root as a name
         if name=='root':
             print('you can\'t use root as a name')
             return
+        # if the node is the root, we can't change its name
         if root==node:
             print('you can\'t change the name of root')
             return
-        
+
         for x in root.children:
             if x.path == node.path:
-                # for i in root.children:
-                #     if name == i.name:
-                #         print('there is another directory with the same name')        
-                #         return   
                 if name in self.ls(root):
                     print('there is another directory with the same name')        
                     return
